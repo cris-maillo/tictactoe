2ebfb1dd93d502121f57dce8263a9a41fcebc8bf
cris-maillo <cristinamaillo.r@gmail.com>
2022-06-07 19:41:47 +0100

    i gave up on basing it on modules and factory functions but now added player toggle

--- script.js
+++ script.js
@@ -1,42 +1,77 @@
+const container = document.getElementById("container");
+const noughts = document.getElementById("noughts");
+const crosses = document.getElementById("crosses");
+crosses.classList = "active";
 
-const gameBoard = (function() {
-    const board = [" ", " ", " "," ", " ", " ", " ", " ", " "];
-    
-    const createBoard = (function() {
-        let container = document.getElementById("container");
-        
-        while (container.firstChild) {
-            container.removeChild(container.lastChild);
-        }
+let board = [
+    "", "", "",
+    "", "", "",
+    "", "", ""];
+let turn = "X";
 
-        for (let i = 0; i < board.length; i++){
-            let square = document.createElement("div");
-            square.id = i;
-            square.className = "square";
-            square.innerHTML = board[i];
-            square.addEventListener("click", addMark);
-            container.appendChild(square);
-            
-        }
-    });
+
+
+const createBoard = function() {
     
-    const addMark = function(){
-        let i = this.id;
-        // check if cell is already used
-        if (board[i] == "X" || board[i] == "O"){
-            console.log("in")
-        }else{
-            board[i] = "X";
-            createBoard();
-        }
-        
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+
+    console.log("turn" + turn)
+
+    for (let i = 0; i < board.length; i++){
+        // create board in DOM, add marks and add function
+        let square = document.createElement("div");
+        square.id = i;
+        square.className = "square";
+        square.innerHTML = board[i];
+        square.addEventListener("click", addMark);
+        container.appendChild(square);
+    }
+};
+
+// 3. check if win / tie
+// 4. check for remaining cells -> tie
+
+const addMark = function(){
+    let i = this.id;
+    // check if cell is already used
+    if (!(board[i] == "X" || board[i] == "O")){
+        board[i] = turn;
+        switchPlayer();
     }
     
-    return {board, createBoard, addMark}
+};
 
-})();
+const switchPlayer = function(){
+    if (turn === "X"){
+        noughts.classList = "active";
+        crosses.classList.remove("active");
+        turn = "O"
+    }else{
+        crosses.classList = "active";
+        noughts.classList.remove("active")
+        turn = "X"
+    }
+    // playerDiv.classList.toggle = "active";
+    createBoard();
+}
 
 
+    // const checkIfWinner = function(){
+    //     const winningFormulas = [
+    //         [1, 2, 3],
+    //         [4, 5, 6],
+    //         [7, 8, 9],
+    //         [1, 4, 7],
+    //         [2, 5, 8],
+    //         [3, 6, 9],
+    //         [1, 5, 9],
+    //         [3, 5, 7],
+    //       ];
+    // }
+    
 
-gameBoard.createBoard();
+createBoard();
+
 
