9cf0d54893ea9e989c68dbd35697d4a968d14d22
cris-maillo <cristinamaillo.r@gmail.com>
2022-07-03 21:51:07 +0100

    new branch for class-based code

--- script.js
+++ script.js
@@ -17,68 +17,76 @@ let startingPlayer = "X";
 
 
 
-const createBoard = function() {
+class Board{
+    constructor(board){
+        this.board = board;
+    }
+
+    createBoard(){
+        while (container.firstChild) {
+            container.removeChild(container.lastChild);
+        }
     
-    while (container.firstChild) {
-        container.removeChild(container.lastChild);
+        for (let i = 0; i < board.length; i++){
+            // create board in DOM, add marks and add function
+            let square = document.createElement("div");
+            square.id = i;
+            square.className = "square";
+            square.innerHTML = board[i];
+            square.addEventListener("click", addMark);
+            container.appendChild(square);
+        }
     }
 
-    for (let i = 0; i < board.length; i++){
-        // create board in DOM, add marks and add function
-        let square = document.createElement("div");
-        square.id = i;
-        square.className = "square";
-        square.innerHTML = board[i];
-        square.addEventListener("click", addMark);
-        container.appendChild(square);
-    }
-
-};
-
-
-function addMark(){
-    let i = this.id;
-    // check if cell is already used
-    if (!(board[i] == "X" || board[i] == "O")){
-        board[i] = turn;
-        if (checkIfWinner()){
-            if (turn == "X"){
-                winner = "Crosses"
-            }else{
-                winner = "Noughts"
+    addMark(){
+        let i = this.id;
+        // check if cell is already used
+        if (!(board[i] == "X" || board[i] == "O")){
+            board[i] = turn;
+            if (checkIfWinner()){
+                if (turn == "X"){
+                    winner = "Crosses"
+                }else{
+                    winner = "Noughts"
+                }
+                outcome.innerHTML = winner +  " Wins";
+                endGame();
             }
-            outcome.innerHTML = winner +  " Wins";
+            switchPlayer();
+        }
+        console.log(checkIfWinner())
+        
+
+        if (isTie()){
+            outcome.innerHTML = "It is a Tie";
+            console.log("tie")
             endGame();
         }
-        switchPlayer();
     }
-    console.log(checkIfWinner())
-    
-
-    if (isTie()){
-        outcome.innerHTML = "It is a Tie";
-        console.log("tie")
-        endGame();
-    }
-
-    
-    
-};
-
-
-const switchPlayer = function(){
-    if (turn === "X"){
-        noughts.classList = "active";
-        crosses.classList.remove("active");
-        turn = "O"
-    }else{
-        crosses.classList = "active";
-        noughts.classList.remove("active")
-        turn = "X"
-    }
-
-    createBoard();
 }
+
+class Player{
+    constructor(){
+        
+    }
+
+    switchPlayer(){
+        if (turn === "X"){
+            noughts.classList = "active";
+            crosses.classList.remove("active");
+            turn = "O"
+        }else{
+            crosses.classList = "active";
+            noughts.classList.remove("active")
+            turn = "X"
+        }
+    
+        createBoard();
+    }
+}
+
+
+
 
 function isTie() {
     for (let i = 0; i < 9; i++) {
